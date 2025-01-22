2c59eeb8da99e15398ea65bcc0a53e5acec0d107
danielcalado07 <daniel.calado.profissional@gmail.com>
2025-01-21 21:43:18 -0300

    ajuste no envio de email

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import About from './pages/about';
 import TechnicalSkills from './pages/technicalSkills';
 import ProfessionalExperiences from './pages/professionalExperiences';
 import ControlledCarousel from './pages/certificates';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import axios from 'axios';
 import Contacts from './pages/Contacts';
 
 function App() {
   const aboutRef = useRef<HTMLDivElement>(null);
   const skillsRef = useRef<HTMLDivElement>(null);
   const contactsRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const sendEmail = async () => {
+      const conteudoEmail = "Portifólio acessado as " + new Date().toLocaleString();
+      await axios.post(`https://backend-leiloame.vercel.app/email`, {
+        email: 'danielcalado159@gmail.com',
+        subject: 'Portifólio acessado',
+        html: conteudoEmail,
+        text: "Portifólio acessado"
+      });
+    };
+    sendEmail();
+  }, []);
 
   const handleScrollToAbout = () => {
     aboutRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -25,16 +38,16 @@ function App() {
 
   return (
     <div className="App">
-        <NavBar
+      <NavBar
         onScrollToAbout={handleScrollToAbout}
         onScrollToProjects={handleScrollToProjects}
         onScrollToContacts={handleScrollToContacts}
       />
-        <About ref = {aboutRef}/>
-        <TechnicalSkills/>
-        <ProfessionalExperiences ref = {skillsRef}/>
-        <ControlledCarousel />
-        <Contacts ref = {contactsRef}/>
+      <About ref={aboutRef} />
+      <TechnicalSkills />
+      <ProfessionalExperiences ref={skillsRef} />
+      <ControlledCarousel />
+      <Contacts ref={contactsRef} />
     </div>
   );
 }
